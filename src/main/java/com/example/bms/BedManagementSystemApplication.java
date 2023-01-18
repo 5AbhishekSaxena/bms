4ca8ce11ac44396aadddc96fbe65c9fbc42a964a
Abhishek Saxena <5abhisheksaxena@gmail.com>
2023-01-18 16:16:00 +0530

    init commit

---
+++ src/main/java/com/example/bms/BedManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BedManagementSystemApplication.class, args);
+	}
+
+}
